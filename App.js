61bc64d7fd9b82ec7d75bc341cc29c485c44529b
DanielVarjaskéri <varjaskerid@gmail.com>
2023-02-25 16:45:42 +0100

    1st commet

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,SafeAreaView } from 'react-native';
 
 export default function App() {
+
+  const handlePress = () => console.log("PRessed");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+    
+
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={2} onPress={ handlePress }>
+        My First App long long long hasdb hasdasds hhiu yasdasda hbliuhhliuasd iuhbasdbas
+        
+      </Text>
+      
+      
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#ffff',
+    
   },
 });
