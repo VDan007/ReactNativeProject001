1613db832320b10078276e8577b66bee294c6e53
DanielVarjaskéri <varjaskerid@gmail.com>
2023-02-25 17:18:55 +0100

    experimenting

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View,SafeAreaView, Image,TouchableWithoutFeedback,TouchableOpacity,TouchableHighlight, Button } from 'react-native';
 
 export default function App() {
 
   const handlePress = () => console.log("PRessed");
+  const imageTap = () => console.log("imgTapped");
+  const btnTap = () => alert("btn Tapped");
 
   return (
 
@@ -11,9 +13,24 @@ export default function App() {
 
     <SafeAreaView style={styles.container}>
       <Text numberOfLines={2} onPress={ handlePress }>
-        My First App long long long hasdb hasdasds hhiu yasdasda hbliuhhliuasd iuhbasdbas
-        
+        My First App 
       </Text>
+      <TouchableHighlight onPress={imageTap}>
+        <Image 
+          
+          source={ {
+          width: 200,
+          height: 200,
+          uri: "https://picsum.photos/200"} }/>
+      </TouchableHighlight>
+
+      <View styles={styles.container02}>
+            <Button
+              title="Click Me"
+              onPress={btnTap}
+              color="orange"
+            />
+      </View>    
       
       
       <StatusBar style="auto" />
@@ -28,5 +45,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: '#ffff',
     
+  },
+  container02: {
+    flex:1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: 'lightgray',
   },
 });
