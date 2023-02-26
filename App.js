cebaa63f0a1983ed971b44988f727d952fb13daa
DanielVarjaskéri <varjaskerid@gmail.com>
2023-02-26 12:24:28 +0100

    experimenting

--- App.js
+++ App.js
@@ -1,55 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,SafeAreaView, Image,TouchableWithoutFeedback,TouchableOpacity,TouchableHighlight, Button } from 'react-native';
+
+import { StyleSheet, Text, View,SafeAreaView,
+   Image,TouchableWithoutFeedback,TouchableOpacity,
+   TouchableHighlight, Button, Alert,
+   Platform, StatusBar,ImageBackground} from 'react-native';
+   
 
 export default function App() {
 
-  const handlePress = () => console.log("PRessed");
-  const imageTap = () => console.log("imgTapped");
-  const btnTap = () => alert("btn Tapped");
+  
 
   return (
+    <View style={ {backgroundColor: "white",
+                  flex: 1,
+                  } }>
+      
+      <ImageBackground
+       source={require("./assets/background.jpg")} 
+       style={{
+        width: "100%",
+        height: "100%",
+       }
+       }
+      >           
+      
 
+          <View
+            style={{
+              borderWidth: 1,
+              borderColor: "red"
+            }}
+          >
+              <Image
+                source={require("./assets/logo-red.png")}
+                style={{
+                  width: 50,
+                  height: 50,
+
+                }}
+              >
+
+              </Image>
+          </View>
+       </ImageBackground>     
+
+    </View>
     
 
-    <SafeAreaView style={styles.container}>
-      <Text numberOfLines={2} onPress={ handlePress }>
-        My First App 
-      </Text>
-      <TouchableHighlight onPress={imageTap}>
-        <Image 
-          
-          source={ {
-          width: 200,
-          height: 200,
-          uri: "https://picsum.photos/200"} }/>
-      </TouchableHighlight>
-
-      <View styles={styles.container02}>
-            <Button
-              title="Click Me"
-              onPress={btnTap}
-              color="orange"
-            />
-      </View>    
-      
-      
-      <StatusBar style="auto" />
-    </SafeAreaView>
+   
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: '#ffff',
-    
-  },
-  container02: {
-    flex:1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: 'lightgray',
-  },
-});
+
