16881fa33d19e5a828b04475cd57a898951cc326
DanielVarjaskéri <varjaskerid@gmail.com>
2023-02-26 12:45:26 +0100

    first decent look

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 import { StyleSheet, Text, View,SafeAreaView,
    Image,TouchableWithoutFeedback,TouchableOpacity,
    TouchableHighlight, Button, Alert,
-   Platform, StatusBar,ImageBackground} from 'react-native';
+   Platform, StatusBar,ImageBackground,} from 'react-native';
    
 
 export default function App() {
@@ -26,21 +26,53 @@ export default function App() {
 
           <View
             style={{
-              borderWidth: 1,
-              borderColor: "red"
+              alignItems: "center",
+              top: 80,
+              
             }}
           >
               <Image
                 source={require("./assets/logo-red.png")}
                 style={{
-                  width: 50,
-                  height: 50,
+                  width: 80,
+                  height: 80,
 
                 }}
               >
 
               </Image>
+              <Text>Sell What You Don't Need</Text>
           </View>
+
+        <View
+            style={{
+              position: "absolute",
+              bottom:0,
+              left:0,
+              width: "100%"
+            }}
+          >      
+            <View
+              style={{
+                backgroundColor: "#fc5c65",
+                height: 65,
+                
+              }}
+            >
+
+            </View>
+
+            <View
+              style={{
+                backgroundColor: "#4ECDC4",
+                height: 65,
+              }}
+            >
+
+            </View>
+        </View>  
+
+
        </ImageBackground>     
 
     </View>
