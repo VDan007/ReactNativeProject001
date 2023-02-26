330928dbd8ca4c5caa97dae72722b482c0cef40e
DanielVarjaskéri <varjaskerid@gmail.com>
2023-02-26 17:16:57 +0100

    structure learned

---
+++ assets/screens/ViewImageScreen.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View,SafeAreaView,
+   Image,TouchableWithoutFeedback,TouchableOpacity,
+   TouchableHighlight, Button, Alert,
+   Platform, StatusBar,ImageBackground,} from 'react-native';
+
+
+   function ViewImageScreen(){
+        return(
+          <View
+            style={{
+               flex:1,
+               backgroundColor: "black",
+            }}
+          >
+
+          </View>
+        );
+   }
+
+   export default ViewImageScreen;
